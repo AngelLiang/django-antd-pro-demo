3061aaa94c00948ccfac2354328c565b8a6a76af
AngelLiang <pl01665077@163.com>
2020-11-14 14:34:36 +0800

    feat: 用户高级表格添加新建按钮

--- frontend/src/pages/user/table/index.jsx
+++ frontend/src/pages/user/table/index.jsx
@@ -1,10 +1,18 @@
 import { PageContainer } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
+import { Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 export default () => {
     return (
       <PageContainer>
-        <ProTable />
+        <ProTable 
+          toolBarRender={() => [
+            <Button type="primary">
+              <PlusOutlined /> 新建
+            </Button>,
+          ]}
+        />
     </PageContainer>
     )
   };
