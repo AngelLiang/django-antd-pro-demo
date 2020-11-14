9a43a43c07dce7bfbcf4082c9e68f57551070a0d
AngelLiang <pl01665077@163.com>
2020-11-14 14:36:56 +0800

    feat: 用户高级表格添加用户名列

--- frontend/src/pages/user/table/index.jsx
+++ frontend/src/pages/user/table/index.jsx
@@ -4,6 +4,17 @@ import { Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 export default () => {
+
+  const columns = [
+    {
+      title: '用户名',
+      dataIndex: 'username',
+      tip: '用户名',
+      render: (dom, entity) => {
+        return <a onClick={() => setRow(entity)}>{dom}</a>;
+      },
+    },
+  ]
     return (
       <PageContainer>
         <ProTable 
@@ -12,6 +23,7 @@ export default () => {
               <PlusOutlined /> 新建
             </Button>,
           ]}
+          columns={columns}
         />
     </PageContainer>
     )
