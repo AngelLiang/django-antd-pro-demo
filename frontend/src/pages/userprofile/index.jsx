11bfe13d84f028ab4177f5b51e88c25bb3d28bcb
E7EE477C11C98D403F318118733EAA5C <E7EE477C11C98D403F318118733EAA5C@i-search.com.cn>
2020-11-25 17:52:18 +0800

    feat: 前端更新个人详情页面

--- frontend/src/pages/userprofile/index.jsx
+++ frontend/src/pages/userprofile/index.jsx
@@ -1,20 +1,144 @@
 import { Component } from 'react';
 import { GridContent } from '@ant-design/pro-layout';
-import { connect } from 'umi';
+import { connect, FormattedMessage } from 'umi';
+import { Menu } from 'antd';
 import styles from './style.less';
 import BaseView from './components/base';
 
+const { Item } = Menu;
+
 class UserProfile extends Component {
+  main = undefined;
+
+  constructor(props) {
+    super(props);
+    const menuMap = {
+      base: (
+        <FormattedMessage id="accountandsettings.menuMap.basic" defaultMessage="个人信息" />
+      ),
+      // security: (
+      //   <FormattedMessage
+      //     id="accountandsettings.menuMap.security"
+      //     defaultMessage="Security Settings"
+      //   />
+      // ),
+      // binding: (
+      //   <FormattedMessage
+      //     id="accountandsettings.menuMap.binding"
+      //     defaultMessage="Account Binding"
+      //   />
+      // ),
+      // notification: (
+      //   <FormattedMessage
+      //     id="accountandsettings.menuMap.notification"
+      //     defaultMessage="New Message Notification"
+      //   />
+      // ),
+    };
+    this.state = {
+      mode: 'inline',
+      menuMap,
+      selectKey: 'base',
+    };
+  }
+
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'user/fetchCurrentUser',
+    });
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+
+  resize = () => {
+    if (!this.main) {
+      return;
+    }
+
+    requestAnimationFrame(() => {
+      if (!this.main) {
+        return;
+      }
+
+      let mode = 'inline';
+      const { offsetWidth } = this.main;
+
+      if (this.main.offsetWidth < 641 && offsetWidth > 400) {
+        mode = 'horizontal';
+      }
+
+      if (window.innerWidth < 768 && offsetWidth > 400) {
+        mode = 'horizontal';
+      }
+
+      this.setState({
+        mode,
+      });
+    });
+  };
+  getMenu = () => {
+    const { menuMap } = this.state;
+    return Object.keys(menuMap).map((item) => <Item key={item}>{menuMap[item]}</Item>);
+  };
+  selectKey = (key) => {
+    this.setState({
+      selectKey: key,
+    });
+  };
+
+  renderChildren = () => {
+    const { selectKey } = this.state;
+
+    switch (selectKey) {
+      case 'base':
+        return <BaseView />;
+
+      // case 'security':
+      //   return <SecurityView />;
+
+      // case 'binding':
+      //   return <BindingView />;
+
+      // case 'notification':
+      //   return <NotificationView />;
+
+      default:
+        break;
+    }
+
+    return null;
+  };
 
   render() {
     console.log(this.props)
 
+    const { mode, selectKey } = this.state;
+
     return (
       <GridContent>
-        <div className={styles.leftMenu}>
-        </div>
-        <div className={styles.right}>
-          <BaseView></BaseView>
+        <div
+          className={styles.main}
+          ref={(ref) => {
+            if (ref) {
+              this.main = ref;
+            }
+          }}
+        >
+          <div className={styles.leftMenu}>
+            <Menu mode={mode} selectedKeys={[selectKey]} onClick={({ key }) => this.selectKey(key)}>
+              {this.getMenu()}
+            </Menu>
+          </div>
+          <div className={styles.right}>
+            <BaseView></BaseView>
+          </div>
         </div>
       </GridContent>
     );
